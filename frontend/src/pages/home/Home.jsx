cb1cc523e35cbb1f22f1ddd277c54ee94e116c36
smRid <omorsk911@gmail.com>
2025-07-08 04:28:29 +0600

    Created TrendingProducts component to showcase a list of products

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from './Banner'
 import Categories from './Categories'
 import HeroSection from './HeroSection'
+import TrendingProducts from '../shop/TrendingProducts'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Banner />
       <Categories />
       <HeroSection />
+      <TrendingProducts />
     </>
   )
 }
