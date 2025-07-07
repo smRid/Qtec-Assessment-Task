ed1b86a5ea02991ac4d50bcd44d6c37543f9f04d
smRid <omorsk911@gmail.com>
2025-07-07 22:30:39 +0600

    Add Banner component and integrate into Home page

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Home from "../pages/home/Home";
 
 const router = createBrowserRouter([ 
     {
@@ -8,7 +9,7 @@ const router = createBrowserRouter([
         children: [
             {
                 path: "/",
-                element: <div>Home Page</div>
+                element: <Home/>
             }
         ]
     }
