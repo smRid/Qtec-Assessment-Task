be8faf806e7c95834e51dcad421d9b8fbc1a9c39
smRid <omorsk911@gmail.com>
2025-07-08 05:01:03 +0600

    Add Footer and PromoBanner components

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Categories from './Categories'
 import HeroSection from './HeroSection'
 import TrendingProducts from '../shop/TrendingProducts'
 import DealsSection from './DealsSection'
+import PromoBanner from './PromoBanner'
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <HeroSection />
       <TrendingProducts />
       <DealsSection />
+      <PromoBanner/>
     </>
   )
 }
