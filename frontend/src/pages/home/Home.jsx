f56aa2f4016b40d56dca70b55af7a01308dab25c
smRid <omorsk911@gmail.com>
2025-07-08 00:41:33 +0600

    Add HeroSection component with card display for trending products

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import Categories from './Categories'
+import HeroSection from './HeroSection'
 
 const Home = () => {
   return (
     <>
       <Banner />
       <Categories />
+      <HeroSection />
     </>
   )
 }
